0849a460e304c02e632c297a0edbb8f0e8225fe9
Andrew Roberts <andrewroberts@Andrews-MBP-29.wowway.com>
2017-08-13 19:40:36 -0500

    Added sidewinder algorithm

--- Mazes/maze_generation_algorithms.py
+++ Mazes/maze_generation_algorithms.py
@@ -1,7 +1,7 @@
 #
 # maze_generation_algorithms.py
 # Various algorithms to generate a random maze
-# Last Modified: 8/12/2017
+# Last Modified: 8/13/2017
 # Modified By: Andrew Roberts
 #
 
@@ -23,3 +23,29 @@ def binary_search(grid):
 			cell_neighbors = grid.cell_at(i, j).current_neighbors(["north", "east"])	
 			to_link = np.random.choice(cell_neighbors) 
 			grid.cell_at(i, j).link(to_link)	
+
+def sidewinder(grid):
+	# Northern row
+	for j, cell in enumerate(grid.row_at(0)[:-1]):
+		cell.link(grid.cell_at(0, j+1))
+
+	for i, row in enumerate(grid.grid):
+		if i == 0:
+			continue
+		cell_run = [grid.cell_at(i, 0)]
+		for j, cell in enumerate(row):
+			if j == grid.columns-1:
+				cell_chosen = np.random.choice(cell_run)
+				cell_chosen.link(cell_chosen.neighbors["north"])
+				break	
+
+			expand_run = np.random.choice([0, 1])
+			if expand_run:
+				cell.link(grid.cell_at(i, j+1))
+				cell_run.append(cell)
+			else:
+				cell_chosen = np.random.choice(cell_run)
+				cell_chosen.link(cell_chosen.neighbors["north"])
+				cell_run = [grid.cell_at(i, j+1)]
+				
+	
